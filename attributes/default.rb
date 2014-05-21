f3d921b7fafb207f578ce60eeef36425612fe4aa
Nguyen Viet Tien <viettienn@gmail.com>
2014-05-21 16:25:52 +0800

    Doesn't make sense to use empty api key as default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,7 @@
 default[:td_agent][:user] = 'root'
 default[:td_agent][:group] = 'root'
 default[:td_agent][:install_dir] = '/etc/td-agent/'
-
-default[:td_agent][:api_key] = ''
+default[:td_agent][:version] = '1.1.19'
 
 default[:td_agent][:plugins] = []
 
-default[:td_agent][:version] = '1.1.19'

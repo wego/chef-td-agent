4f326ec8199fcb312bf3ac3ea60112b56debadd5
Nguyen Viet Tien <viettienn@gmail.com>
2014-05-21 16:22:31 +0800

    Always use include and no need for default config

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,6 +6,4 @@ default[:td_agent][:api_key] = ''
 
 default[:td_agent][:plugins] = []
 
-default[:td_agent][:includes] = false
-default[:td_agent][:default_config] = true
 default[:td_agent][:version] = '1.1.19'

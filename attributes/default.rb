177b995c8d5040a6d524914bcf4259ec638d2079
Nguyen Viet Tien <viettienn@gmail.com>
2014-05-22 14:55:02 +0800

    Change default install dir as this is hardcode in td-agent package

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default[:td_agent][:user] = 'root'
 default[:td_agent][:group] = 'root'
-default[:td_agent][:install_dir] = '/usr/local/fluentd'
+default[:td_agent][:install_dir] = '/etc/td-agent'
 default[:td_agent][:version] = '1.1.19'
 
 default[:td_agent][:plugins] = []
